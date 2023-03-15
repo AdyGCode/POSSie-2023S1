772b80109dfb199093fa9c5d5306ac14adeba811
Adrian Gould <Adrian.Gould@nmtafe.wa.edu.au>
2023-03-15 20:00:04 +0800

    feat: Baby Shark
    - step 2 - functions

--- Session-06/live_code.py
+++ Session-06/live_code.py
@@ -1,47 +1,87 @@
 # Live Coding 2023-03-15
+def baby_shark():
+    print("Baby Shark, doo-doo, doo-doo")
+    print("Baby Shark, doo-doo, doo-doo")
+    print("Baby Shark, doo-doo, doo-doo")
+    print("Baby Shark")
+    print()
 
-print("Baby Shark, doo-doo, doo-doo")
-print("Baby Shark, doo-doo, doo-doo")
-print("Baby Shark, doo-doo, doo-doo")
-print("Baby Shark")
-print()
-print("Mommy Shark, doo-doo, doo-doo")
-print("Mommy Shark, doo-doo, doo-doo")
-print("Mommy Shark, doo-doo, doo-doo")
-print("Mommy Shark")
-print()
-print("Daddy Shark, doo-doo, doo-doo")
-print("Daddy Shark, doo-doo, doo-doo")
-print("Daddy Shark, doo-doo, doo-doo")
-print("Daddy Shark")
-print()
-print("Grandma Shark, doo-doo, doo-doo")
-print("Grandma Shark, doo-doo, doo-doo")
-print("Grandma Shark, doo-doo, doo-doo")
-print("Grandma Shark")
-print()
-print("Grandpa Shark, doo-doo, doo-doo")
-print("Grandpa Shark, doo-doo, doo-doo")
-print("Grandpa Shark, doo-doo, doo-doo")
-print("Grandpa Shark")
-print()
-print("Let's go hunt, doo-doo, doo-doo")
-print("Let's go hunt, doo-doo, doo-doo")
-print("Let's go hunt, doo-doo, doo-doo")
-print("Let's go hunt")
-print()
-print("Run away, doo-doo, doo-doo")
-print("Run away, doo-doo, doo-doo")
-print("Run away, doo-doo, doo-doo")
-print("Run away (ah!)")
-print()
-print("Safe at last, doo-doo, doo-doo")
-print("Safe at last, doo-doo, doo-doo")
-print("Safe at last, doo-doo, doo-doo")
-print("Safe at last (phew)")
-print()
-print("It's the end, doo-doo, doo-doo")
-print("It's the end, doo-doo, doo-doo")
-print("It's the end, doo-doo, doo-doo")
-print("It's the end")
-print()
+
+def mommy_shark():
+    print("Mommy Shark, doo-doo, doo-doo")
+    print("Mommy Shark, doo-doo, doo-doo")
+    print("Mommy Shark, doo-doo, doo-doo")
+    print("Mommy Shark")
+    print()
+
+
+def daddy_shark():
+    print("Daddy Shark, doo-doo, doo-doo")
+    print("Daddy Shark, doo-doo, doo-doo")
+    print("Daddy Shark, doo-doo, doo-doo")
+    print("Daddy Shark")
+    print()
+
+
+def grandma_shark():
+    print("Grandma Shark, doo-doo, doo-doo")
+    print("Grandma Shark, doo-doo, doo-doo")
+    print("Grandma Shark, doo-doo, doo-doo")
+    print("Grandma Shark")
+    print()
+
+
+def grandad_shark():
+    print("Grandpa Shark, doo-doo, doo-doo")
+    print("Grandpa Shark, doo-doo, doo-doo")
+    print("Grandpa Shark, doo-doo, doo-doo")
+    print("Grandpa Shark")
+    print()
+
+
+def hunting_shark():
+    print("Let's go hunt, doo-doo, doo-doo")
+    print("Let's go hunt, doo-doo, doo-doo")
+    print("Let's go hunt, doo-doo, doo-doo")
+    print("Let's go hunt")
+    print()
+
+
+def run_away_shark():
+    print("Run away, doo-doo, doo-doo")
+    print("Run away, doo-doo, doo-doo")
+    print("Run away, doo-doo, doo-doo")
+    print("Run away (ah!)")
+    print()
+
+
+def safe_shark():
+    print("Safe at last, doo-doo, doo-doo")
+    print("Safe at last, doo-doo, doo-doo")
+    print("Safe at last, doo-doo, doo-doo")
+    print("Safe at last (phew)")
+    print()
+
+
+def end_shark():
+    print("It's the end, doo-doo, doo-doo")
+    print("It's the end, doo-doo, doo-doo")
+    print("It's the end, doo-doo, doo-doo")
+    print("It's the end")
+    print()
+
+
+def sing_along():
+    baby_shark()
+    mommy_shark()
+    daddy_shark()
+    grandma_shark()
+    grandad_shark()
+    hunting_shark()
+    run_away_shark()
+    safe_shark()
+    end_shark()
+
+
+if __name__ == "__main__":
+    sing_along()
