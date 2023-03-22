f7cebe44cc05622e2c551327ac5c3513e0d6d260
Adrian Gould <Adrian.Gould@nmtafe.wa.edu.au>
2023-03-22 20:15:54 +0800

    feat: Session 06
    - Live Code

--- Session-06/live_code.py
+++ Session-06/live_code.py
@@ -1,15 +1,16 @@
 # Live Coding 2023-03-15
-def lyric(activity):
-    action_ends = {
-        "Run away": "ah!",
-        "Safe at last": "phew",
-    }
+def lyric(activity_item):
+    activity = activity_item
+    action_end = ""
+    if isinstance(activity_item, dict):
+        for key, value in activity_item.items():
+            activity = key
+            action_end = value
     line_end = ("doo-doo, " * 3)[:-2]
     for lyric_line in range(0, 3):
         print(f"{activity}, {line_end}")
-    if activity in action_ends:
-        activity += f" ({action_ends[activity]})"
-    print(f"{activity}")
+        activity_line = f"{activity} ({action_end})"
+    print(f"{activity_line}")
     print()
 
 
@@ -17,8 +18,8 @@ def sing_along():
     action_list = ["Baby Shark", "Mommy Shark", "Daddy Shark",
                    "Grandma Shark", "Grandad Shark",
                    "Shakespear Shark", "We all play",
-                   "Let's go hunt",
-                   "Run away", "Safe at last", "It's the end"]
+                   "Let's go hunt", {"Run away": "ah!"},
+                   {"Safe at last": "phew"}, "It's the end"]
     for action in action_list:
         lyric(action)
 
